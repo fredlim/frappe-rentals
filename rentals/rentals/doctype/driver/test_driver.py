14697a00e2658aa0247495d84ba04c379139c139
Frederick Lim <hungwai.lim@gmail.com>
2025-08-17 10:29:30 +0000

    feat: Vehicle Type

--- rentals/rentals/doctype/driver/test_driver.py
+++ rentals/rentals/doctype/driver/test_driver.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2025, Erous and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestDriver(FrappeTestCase):
-	pass
+	def test_full_name_correctly_set(self):
+		test_driver = frappe.new_doc("Driver")
+		test_driver.first_name = "Bill"
+		test_driver.last_name = "Gates"
+		test_driver.license_number = "US123456"
+		test_driver.save()
+
+		self.assertEqual(test_driver.full_name, "Bill Gates")
+
+	def test_full_name_correctly_set_when_last_name_not_set(self):
+		test_driver = frappe.new_doc("Driver")
+		test_driver.first_name = "Bill"
+		test_driver.license_number = "US123456"
+		test_driver.save()
+
+		self.assertEqual(test_driver.full_name, "Bill")
